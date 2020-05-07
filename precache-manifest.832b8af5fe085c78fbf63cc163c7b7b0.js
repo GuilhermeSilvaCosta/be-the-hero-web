e4bd71cb02784fb0c8fe94b8e960fe64558df6ea
Guilherme Costa <tec.guilherme.dsc@gmail.com>
2020-05-07 15:51:42 -0300

    Updates

---
+++ precache-manifest.832b8af5fe085c78fbf63cc163c7b7b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc373b64e9e988a031d582b3832b484",
+    "url": "/be-the-hero-web/index.html"
+  },
+  {
+    "revision": "9459489a04280823053a",
+    "url": "/be-the-hero-web/static/css/main.722a8ff4.chunk.css"
+  },
+  {
+    "revision": "85857c74afea54815a5f",
+    "url": "/be-the-hero-web/static/js/2.a36d1617.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/be-the-hero-web/static/js/2.a36d1617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9459489a04280823053a",
+    "url": "/be-the-hero-web/static/js/main.1066c8ca.chunk.js"
+  },
+  {
+    "revision": "1bec5b8038ae42c7dbf0",
+    "url": "/be-the-hero-web/static/js/runtime-main.fab13d70.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/be-the-hero-web/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/be-the-hero-web/static/media/logo.7eea718b.svg"
+  }
+]);
